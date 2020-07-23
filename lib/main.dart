d1745aab0d97af9917eb8f756405bc17ece08a13
Eugênio Barros <eugenio.barros.jr@gmail.com>
2020-07-23 11:47:57 -0300

    feat(main): :tada: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
