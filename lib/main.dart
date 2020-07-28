79ff25b8829d154c4f6bdc1fc9b6631cc52beda3
Eugênio Barros <eugenio.barros.jr@gmail.com>
2020-07-28 10:03:21 -0300

    feature(Images): add images

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,31 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left button got pressed.');
+              },
+              child: Image.asset(
+                'images/dice1.png',
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right button got pressed');
+              },
+              child: Image.asset(
+                'images/dice1.png',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
